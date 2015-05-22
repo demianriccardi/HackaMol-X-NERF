3d6697a287ca372273a77357a53b58af43049c38
demianriccardi <demianriccardi@gmail.com>
2015-05-22 11:51:53 -0400

    sp3 with one bond done

--- hbuild.pl
+++ hbuild.pl
@@ -19,19 +19,48 @@ sub hbuild_sp3{
 
   my ($mol, $iat) = (shift,shift);
 
+  #get atom of interest
   my $atom = $mol->get_atoms($iat);
-  
+ 
+  #find all the bonds to the atom of interest 
   my @bonds = $hack->find_bonds_brute(
                                   bond_atoms => [$atom],
                                   candidates => [$mol->all_atoms],
                                   fudge      => 0.45,
                                   max_bonds  => 6,
   );
+ 
   $mol->push_bonds(@bonds);
-  my $number_of_hyd = 4 - $atom->bond_count;
-  
+  #my $number_of_hyd = 4 - $atom->bond_count;
 
-  print $number_of_hyd . "\n"; 
+  #add some number of hydrogens depending on number of bonds to atom of interest.
+  if ($atom->bond_count == 1){
+    my $at_bound = $bonds[0]->get_atoms(1);
+
+    #find all bonds from the bound atom to establish the reference frame
+    my @b_bonds = $hack->find_bonds_brute(
+                                  bond_atoms => [$at_bound],
+                                  candidates => [$mol->all_atoms],
+                                  fudge      => 0.45,
+                                  max_bonds  => 6,
+    );
+    # this may be cheating.
+    pop @b_bonds;
+
+    # map out the math::vector::real xyz for each of the atoms of interest 
+    my ($a,$b,$c) = map{$_->get_atoms(1)->xyz} (@bonds,@b_bonds);
+    
+    # return a math::vector::real xyz for each of the 3 hydrogens
+    my $h1 = $bld->extend_abc($b, $a, $atom->xyz, 1.09, 109, 0);
+    my $h2 = $bld->extend_abc($h1,$a, $atom->xyz, 1.09, 109, 120);
+    my $h3 = $bld->extend_abc($h1,$a, $atom->xyz, 1.09, 109, -120);
+
+    $mol->push_atoms(
+                      map{HackaMol::Atom->new(Z => 1, coords => [$_])} ($h1,$h2,$h3)
+    );  
+    $mol->print_xyz;
+  }
+
   exit;
  
   print Dumper $mol;exit;
