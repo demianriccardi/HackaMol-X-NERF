e6e2958c72376e9603e0289e0d99f2249479184d
Demian Riccardi <demianriccardi@gmail.com>
2015-05-22 12:47:48 -0400

    fixed build_sp3

--- hbuild.pl
+++ hbuild.pl
@@ -14,6 +14,7 @@ my $hack = HackaMol->new;
 my $mol = HackaMol->new->read_file_mol(shift);
 
 my $new_mol = hbuild_sp3($mol,4);
+$new_mol->print_xyz;
 
 sub hbuild_sp3{
 
@@ -30,11 +31,11 @@ sub hbuild_sp3{
                                   max_bonds  => 6,
   );
  
-  $mol->push_bonds(@bonds);
-  #my $number_of_hyd = 4 - $atom->bond_count;
-
+  if (@bonds > 3){
+    print "already at least four bonds on atom $iat\n";
+  }
+  elsif(@bonds == 1){ 
   #add some number of hydrogens depending on number of bonds to atom of interest.
-  if ($atom->bond_count == 1){
     my $at_bound = $bonds[0]->get_atoms(1);
 
     #find all bonds from the bound atom to establish the reference frame
@@ -57,26 +58,27 @@ sub hbuild_sp3{
 
     $mol->push_atoms(
                       map{HackaMol::Atom->new(Z => 1, coords => [$_])} ($h1,$h2,$h3)
-    );  
-    $mol->print_xyz;
+    );
+    return $mol;  
   }
-
-  exit;
- 
-  print Dumper $mol;exit;
-  #print Dumper V(1,1,0) -> versor; exit;
-  my $h1 = HackaMol::Atom->new(
-                                symbol => 'H',
-                                coords => [
-                     $bld->extend_a($atom->xyz,'1.09', V(1,1,1))
-                                ] 
-                               );  
-  my $new_mol = HackaMol::Molecule->new(atoms=>[$atom, $h1]);
-  $new_mol->print_xyz;
-  say $h1->distance($atom);
-
-#print Dumper $h1;
-#print Dumper $atom;
+  elsif(@bonds == 2){
+    my ($a,$b) = map {$_->get_atoms(1)->xyz} @bonds;
+    my $h1 = $bld->extend_abc($b, $a, $atom->xyz, 1.09, 109,  120);
+    my $h2 = $bld->extend_abc($b, $a, $atom->xyz, 1.09, 109, -120);
+    $mol->push_atoms(
+                      map{HackaMol::Atom->new(Z => 1, coords => [$_])} ($h1,$h2)
+    );
+    return $mol;
+  }
+  elsif(@bonds == 3){
+    my ($a,$b,$c) = map {$_->get_atoms(1)->xyz} @bonds;
+    my $h1 = $bld->extend_abc($b, $a, $atom->xyz, 1.09, 109,  120);
+    if ($h1->dist($c) < 1.){
+      $h1 = $bld->extend_abc($b, $a, $atom->xyz, 1.09, 109,  -120);
+    }
+    $mol->push_atoms( HackaMol::Atom->new(Z => 1, coords => [$h1]) );
+    return $mol;
+  }
   
 }
 
