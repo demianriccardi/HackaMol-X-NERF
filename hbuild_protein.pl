72a6361db7f9650e45eb8451b0631b51d15ecf80
Demian Riccardi <demianriccardi@gmail.com>
2016-04-21 16:40:26 -0400

    old untracked script

---
+++ hbuild_protein.pl
@@ -0,0 +1,133 @@
+#!/usr/bin/env perl
+use Modern::Perl;
+use HackaMol;
+use HackaMol::X::NERF;
+use Math::Vector::Real;
+use Carp;
+use Data::Dumper;
+
+#create an extension builder
+my $bld  = HackaMol::X::NERF->new;
+my $hack = HackaMol->new;
+
+#print Dumper $bld; exit;
+
+#my $mol = HackaMol->new->read_file_mol('zmatrices/val.zmat');
+my $mol = HackaMol->new->pdbid_mol('2cba');
+my @fun = $mol->select_atoms( sub{ 
+                                    $_->resname eq 'VAL' 
+                                   # or 
+                                   # $_->resname eq 'ALA'
+                                 }); 
+
+my %ires ;
+my @groups = 
+             $hack->group_by_atom_attr('resid',@fun);
+
+foreach my $group (@groups){
+  next unless ($group->count_atoms > 3);
+  foreach my $atom ($group->all_atoms){
+    hbuild_sp3($group,$atom);
+  }
+}
+
+$mol->print_xyz;
+
+sub hbuild_sp3{
+
+  my ($group, $atom) = (shift,shift);
+
+  #find all the bonds to the atom of interest 
+  my @bonds = $hack->find_bonds_brute(
+                                  bond_atoms => [$atom],
+                                  candidates => [$group->all_atoms],
+                                  fudge      => 0.45,
+                                  max_bonds  => 6,
+  );
+ 
+  if (@bonds > 3){
+    print "already at least four bonds on atom ", $atom->iatom, "\n";
+    return;
+  }
+  elsif(@bonds == 1){ 
+  #add some number of hydrogens depending on number of bonds to atom of interest.
+    my $at_bound = $bonds[0]->get_atoms(1);
+
+    #find all bonds from the bound atom to establish the reference frame
+    my @b_bonds = $hack->find_bonds_brute(
+                                  bond_atoms => [$at_bound],
+                                  candidates => [$group->all_atoms],
+                                  fudge      => 0.45,
+                                  max_bonds  => 6,
+    );
+    # this may be cheating.
+    pop @b_bonds;
+
+    # map out the math::vector::real xyz for each of the atoms of interest 
+    my ($a,$b,$c) = map{$_->get_atoms(1)->xyz} (@bonds,@b_bonds);
+    
+    # return a math::vector::real xyz for each of the 3 hydrogens
+    my $h1 = $bld->extend_abc($b, $a, $atom->xyz, 1.09, 109, 0);
+    my $h2 = $bld->extend_abc($h1,$a, $atom->xyz, 1.09, 109, 120);
+    my $h3 = $bld->extend_abc($h1,$a, $atom->xyz, 1.09, 109, -120);
+
+    $mol->push_atoms(
+                      map{HackaMol::Atom->new(Z => 1, coords => [$_])} ($h1,$h2,$h3)
+    );
+    return;  
+  }
+  elsif(@bonds == 2){
+    my ($a,$b) = map {$_->get_atoms(1)->xyz} @bonds;
+    my $h1 = $bld->extend_abc($b, $a, $atom->xyz, 1.09, 109,  120);
+    my $h2 = $bld->extend_abc($b, $a, $atom->xyz, 1.09, 109, -120);
+    $mol->push_atoms(
+                      map{HackaMol::Atom->new(Z => 1, coords => [$_])} ($h1,$h2)
+    );
+    print "shitshit\n";
+    return ;
+  }
+  elsif(@bonds == 3){
+    my ($a,$b,$c) = map {$_->get_atoms(1)->xyz} @bonds;
+    my $h1 = $bld->extend_abc($b, $a, $atom->xyz, 1.09, 109,  120);
+    #if ($h1->dist($c) < 1.){
+    #  $h1 = $bld->extend_abc($b, $a, $atom->xyz, 1.09, 109,  -120);
+    #}
+    $mol->push_atoms( HackaMol::Atom->new(Z => 1, coords => [$h1]) );
+    return ;
+  }
+  
+}
+# example valine
+#hbuild_isp3_iaibic($mol,4,1,5,6)
+
+sub hbuild_isp3_iaibic{
+
+  my ($mol, $iat, $ia,$ib,$ic) = @_ ;
+
+  croak "pass at least two indices of bound atoms" unless defined($ib);
+
+  #get atom of interest
+  my $atom = $mol->get_atoms($iat);
+  my $ata = $mol->get_atoms($ia);
+ 
+  if (defined ($ic)){
+    my $atb = $mol->get_atoms($ib);
+    my $atc = $mol->get_atoms($ic);
+ 
+    my $h1 = $bld->extend_abc($atb->xyz, $ata->xyz, $atom->xyz, 1.09, 109,  120);
+    if ($h1->dist($atc->xyz) < 1.){
+      $h1 = $bld->extend_abc($b, $a, $atom->xyz, 1.09, 109,  -120);
+    }
+    $mol->push_atoms( HackaMol::Atom->new(Z => 1, coords => [$h1]) );
+    return $mol;
+  }
+  elsif (defined ($ib)){
+    my $atb = $mol->get_atoms($ib);
+    my $h1 = $bld->extend_abc($atb->xyz, $ata->xyz, $atom->xyz, 1.09, 109,   120);
+    my $h2 = $bld->extend_abc($atb->xyz, $ata->xyz, $atom->xyz, 1.09, 109,  -120);
+    $mol->push_atoms( HackaMol::Atom->new(Z => 1, coords => [$h1,$h2]) );
+    return $mol;
+  }
+  
+}
+
